d42b51daf57d9ffa98d32884df593534ccaf1100
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2024-10-13 14:18:49 +0900

    上下左右移動とジャンプを実装

--- Assets/Scripts/Cargi.cs
+++ Assets/Scripts/Cargi.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cargi : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Cargi : MonoBehaviour {
+
+    public float speed;
+    public float jumpHeight;
+    public float gravity;
+    
+    float jumpVelocity = 0f;
+    bool isGround = true;
+    Rigidbody rb;
+    
+    void Start() => rb = GetComponent<Rigidbody>();
+
+    void Update(){
+        Jump();
+        Move();Debug.Log(isGround);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Jump(){
+        if(!isGround) jumpVelocity -= gravity * Time.deltaTime;
+        if(isGround && Input.GetKeyDown(KeyCode.Space)) jumpVelocity = jumpHeight;
     }
+
+    void Move() => rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed,jumpVelocity,Input.GetAxis("Vertical") * speed);
+
+    void OnCollisionEnter(Collision other){
+        if(other.gameObject.tag == "Floor"){
+            isGround = true;
+            jumpVelocity = 0;
+        }
+    }
+
+    void OnCollisionExit(Collision other){
+        if(other.gameObject.tag == "Floor") isGround = false;
+    }
+
 }
